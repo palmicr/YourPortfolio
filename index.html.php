3c76a7fda6994002eb12ad3404d169f6b0ce3ff0
Pálmi C Rúnarsson <pancakeprins@gmail.com>
2016-02-09 14:19:51 +0000

    Add inclide sign

--- index.html.php
+++ index.html.php
@@ -27,7 +27,7 @@
 							<ul>
 								<li><a href="#top" id="top-link" class="skel-layers-ignoreHref"><span class="icon fa-home">Home</span></a></li>
 								<li><a href="#portfolio" id="portfolio-link" class="skel-layers-ignoreHref"><span class="icon fa-th">Portfolio</span></a></li>
-								<li><a href="#contact" id="contact-link" class="skel-layers-ignoreHref"><span class="icon fa-th">Sign In/Out</span></a></li>
+								<li><a href="#contact" id="contact-link" class="skel-layers-ignoreHref"><span class="icon fa-th">Sign In/Up</span></a></li>
 
 							</ul>
 						</nav>
@@ -132,23 +132,11 @@
 					</section>
 
 					<!-- Contact -->
-					<section id="contact" class="four">
-						<div class="container">
 
-							<header>
-								<h2>Sign In / Sign Up</h2>
-							</header>
-
-							<p>Elementum sem parturient nulla quam placerat viverra
-							mauris non cum elit tempus ullamcorper dolor. Libero rutrum ut lacinia
-							donec curae mus. Eleifend id porttitor ac ultricies lobortis sem nunc
-							orci ridiculus faucibus a consectetur. Porttitor curae mauris urna mi dolor.</p>
-
-							
-						</div>
-					</section>
-
-
+					<?php 
+						include "php/sign.php";
+			 		?>
+					
 			</div>
 
 			
---
+++ php/sign.php
@@ -0,0 +1,20 @@
+<?php
+
+echo '<section id="contact" class="four">
+						<div class="container">
+
+							<header>
+								<h2>Sign In / Sign Up</h2>
+							</header>
+
+							<p>Elementum sem parturient nulla quam placerat viverra
+							mauris non cum elit tempus ullamcorper dolor. Libero rutrum ut lacinia
+							donec curae mus. Eleifend id porttitor ac ultricies lobortis sem nunc
+							orci ridiculus faucibus a consectetur. Porttitor curae mauris urna mi dolor.</p>
+
+							
+						</div>
+					</section>';
+
+
+?>
